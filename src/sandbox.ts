2fbc67431eb5f7a62031fce45d8076d2562ca472
v0nser <raghuvanshivaibhav01@gmail.com>
2023-12-28 22:21:46 +0530

    function basics

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,25 @@
-console.log("hello")
+// let greet = () => {
+//     console.log('hello, world');
+// }
+
+let greet : Function;
+
+
+greet = () => {
+    console.log('hello, again');
+}
+
+const add = (a: number, b: number, c: number|string=10): void => {
+    console.log(a+b);
+    console.log(c);
+}
+
+add(5, 10);
+
+const minus= (a: number, b:number) => {
+    return a+b;
+}
+
+let result = minus(10, 7);
+
+
